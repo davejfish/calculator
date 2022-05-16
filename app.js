fe3800adf7058a55f3052cf38881f384e1a4a31b
Dave Fisher <dave.j.fish@gmail.com>
2022-05-16 13:58:31 -0700

    updated the Calculator functions, functioning normally now. notable bugs inside delete function if you delete after a calculation and inside selectOperator not being able to choose a new operator after picking one. HTML made easier to read.

--- app.js
+++ app.js
@@ -1,35 +1,55 @@
 'use strict'
 
 // build calculator class
+// include things that calculator need to be able to do
+// ie: update the display, clear, append numbers, choose operators...
 class Calculator {
     constructor(prevVal, curVal) {
         this.prevVal = prevVal;
         this.curVal = curVal;
-        this.clear()
+        //this.clear()
     }
 
     clear() {
         this.curVal = '';
         this.prevVal = '';
         this.operator = undefined;
+        console.log(`current value = ${this.curVal}`)
+        console.log(`previous value = ${this.prevVal}`)
+        console.log(`operator = ${this.operator}`)
     }
 
     isNum(val) {
         return!isNaN(val);
     }
 
-    // if the number is a . or curVal already has a . return
+    // if the number is a . AND curVal already has a '.' then exit the function
     // otherwise the current value is appended with the number
     appendNum(number) {
-        console.log('appending number!')
+        //console.log('appending number!')
+        
+        if ((number === '.') && (this.curVal.includes('.'))) {
+            // console.log('----> entered if')
+            return;
+        }
+        
+        this.curVal = this.curVal + number
+
+        // these are my debug logs while making this function
+        
+        // console.log('curVal contains . : ', this.curVal.includes('.'))
+        // console.log('number.value === . : ', (number.value === '.'))
+        console.log('curVal = ', this.curVal)
+        // console.log('number = ', number)
     }
 
-    getDisplayNumber(number) {
-        console.log('displaying number!')
-    }
-
+    // used to update the display, it will always update to whatever the current value is
+    // in calculation the current value is set to the result to display the current answer 
+    // after an operator buttonpress or a equals buttonpress
     changeDisplay() {
-        console.log('changing display!')
+        // console.log('changing display!')
+
+        inputEl.value = this.curVal
   }
 
     // if the curVal is empty string, return
@@ -37,7 +57,25 @@ class Calculator {
     // set prev Val to whatever curVal is, and reset cur Val.
     // set this operator to the operation button pressed
     selectOperator(operator) {
-        console.log('choosing the operator!')
+        // console.log('choosing the operator!')
+
+        console.log('---> selectOperator');
+        console.log('current value = ', this.curVal)
+
+        if (this.curVal === '') {
+            console.log('current value is: \'\' <-----')
+            return;
+        }
+
+        if (this.curVal != '') {
+            console.log('running calculation');
+            this.calculation();
+        }
+
+        this.operator = operator;
+        console.log(`current operator: ${this.operator}`)
+        this.prevVal = this.curVal;
+        this.curVal = '';
     }
 
     // make a variable for the answer
@@ -46,12 +84,62 @@ class Calculator {
     // use switch with this operator, check for each operator and do calc
     // lastly, cur val is result, operator is not defined, prev val is reset
     calculation() {
-        console.log('mathing it up!')
+        //console.log('mathing it up!')
+
+        let result;
+        
+        const current = parseFloat(this.curVal);
+        const previous = parseFloat(this.prevVal);
+        
+        console.log(`current = ${current}, previous = ${previous}`)
+        console.log(`current isNum: ${this.isNum(current)}`)
+        console.log(`previous isNum: ${this.isNum(previous)}`)
+
+        if (isNaN(previous) || isNaN(current)) {
+            console.log('current or prev is NaN')
+            console.log('no calculation ran')
+            return
+        }
+
+        console.log(`current operator: ${this.operator}`)
+        
+        // without the break statements js will run code from the first true result to the end
+        switch (this.operator) {
+            case '/':
+                result = previous / current;
+                break;
+
+            case '*':
+                result = previous * current;
+                break;
+
+            case '-':
+                result = previous - current;
+                break;
+
+            case '+':
+                result = previous + current;
+                break;
+            
+            case 'default':
+                console.log('default no answer')
+                return;
+        }
+
+        this.curVal = result;
+        this.prevVal = '';
+        this.operator = undefined
+
+        console.log('math\'d it up!')
+        console.log(`the current answer is ${result}`)
+        
     }
 
     // reset the current value to the current toString().slice(0, -1)
     delete() {
-        console.log('deleting last input!')
+        // console.log('deleting last input!')
+        this.curVal = this.curVal.toString().slice(0, -1)
+        console.log(`current value: ${this.curVal}`)
     }
 
 }
@@ -74,19 +162,20 @@ const acButton = document.getElementById('acButton')
 
 // new calc object that is a Calculator
 // we need this to keep track of our values
+// since our values in our html buttons are strings, our starting values
+// for prevVal and curVal will be empty strings
 const calc = new Calculator('', '');
 
-// for each eventlisteners
 
-
-// for every i in numButtons, do this
+//  forEach works like this: for every i in numButtons, do this
 // this way I don't need to write code for every button
 
 // number buttons need to know whether or not to append a number or not
 // and if we need to change the display
 numButtons.forEach(button => {
     button.addEventListener('click', () => {
-        console.log(`button number ${button.value} works`)
+        // console.log(`button number ${button.value} works`)
+        
         // we want our calc object to run these functions
         calc.appendNum(button.value);
         calc.changeDisplay();
@@ -95,23 +184,25 @@ numButtons.forEach(button => {
 
 operatorButtons.forEach(button => {
     button.addEventListener('click', () => {
-        console.log(`button operator ${button.value} works`)
+        // console.log(`button operator ${button.value} works`)
         calc.selectOperator(button.value);
         calc.changeDisplay();
     })
 })
 
 equalButton.addEventListener('click', () => {
-    console.log('the calculation button works')
+    // console.log('the calculation button works')
     calc.calculation()
+    calc.changeDisplay()
 })
 
 deleteButton.addEventListener('click', () => {
     console.log('delete button works')
     calc.delete()
+    calc.changeDisplay()
 })
 
 acButton.addEventListener('click', () => {
-    console.log('all clear button works')
+    // console.log('all clear button works')
     calc.clear()
 })
